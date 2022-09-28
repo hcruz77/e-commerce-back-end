827ca3c5317580c1e1487563ca7f6f8ef55f0a4d
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-09-28 19:47:43 -0400

    troubleshoot route errors for product and category id

--- models/index.js
+++ models/index.js
@@ -19,16 +19,18 @@ Category.hasMany(Product, {
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
+    unique: false
   },
-  foreignKey: 'tag_id',
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
+    unique: false
   },
-  foreignKey: 'product_id'
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
